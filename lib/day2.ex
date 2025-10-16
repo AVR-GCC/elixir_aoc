5c8b5580165774728808d996b805f701d414a082
AVR-GCC <ogoun.d@gmail.com>
2025-10-17 02:35:10 +0300

    Day 2 part 2

--- lib/day2.ex
+++ lib/day2.ex
@@ -32,4 +32,27 @@ defmodule Day2 do
     lines = input_to_lines(path)
     Enum.count(lines, &is_safe/1)
   end
+
+  def is_safe_damp([]), do: true
+  def is_safe_damp([_]), do: true
+  def is_safe_damp([_, _]), do: true
+  def is_safe_damp([x, y, z]), do: is_safe_duo(x, y) or is_safe_duo(x, z) or is_safe_duo(y, z)
+  def is_safe_damp([head, neck, torso, butt | tail]) do
+    if is_safe_trio(head, neck, torso) and is_safe_trio(neck, torso, butt) do is_safe_damp([neck, torso, butt | tail])
+    else
+      is_safe([head, neck, torso | tail]) or is_safe([head, torso, butt | tail])  or is_safe([head, neck, butt | tail]) or is_safe([neck, torso, butt | tail])
+    end
+  end
+
+  def is_safe_damp_print(lst) do
+    IO.inspect(lst, charlists: :as_lists)
+    res = is_safe_damp(lst)
+    IO.puts(res)
+    res
+  end
+
+  def part2(path) do
+    lines = input_to_lines(path)
+    Enum.count(lines, &is_safe_damp/1)
+  end
 end
