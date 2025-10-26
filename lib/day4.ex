abdd7a8fb5fb4142a42c036eefcbc65d393049d8
AVR-GCC <ogoun.d@gmail.com>
2025-10-27 00:13:46 +0200

    Day 4 part 2

--- lib/day4.ex
+++ lib/day4.ex
@@ -85,6 +85,31 @@ defmodule Day4 do
     end
   end
 
+  def cross_match_str(mat, {x, y}, str) do
+    height = length(mat)
+    width = length(Enum.at(mat, 0, []))
+    str_len = length(str)
+    distance_to_direction = round((str_len - 1) / 2)
+    down_clear = height >= distance_to_direction + y
+    right_clear = width >= distance_to_direction + x
+    up_clear = y >= distance_to_direction
+    left_clear = x >= distance_to_direction
+    can_check = down_clear and up_clear and left_clear and right_clear
+    if can_check do
+      up_left_to_down_right = match_str_rec(mat, {x - distance_to_direction, y - distance_to_direction}, str, &(&1 + 1), &(&1 + 1))
+      down_right_to_up_left = match_str_rec(mat, {x + distance_to_direction, y + distance_to_direction}, str, &(&1 - 1), &(&1 - 1))
+      up_right_to_down_left = match_str_rec(mat, {x + distance_to_direction, y - distance_to_direction}, str, &(&1 - 1), &(&1 + 1))
+      down_left_to_up_right = match_str_rec(mat, {x - distance_to_direction, y + distance_to_direction}, str, &(&1 + 1), &(&1 - 1))
+      if up_left_to_down_right + down_right_to_up_left > 0 and up_right_to_down_left + down_left_to_up_right > 0 do
+        1
+      else
+        0
+      end
+    else
+      0
+    end
+  end
+
   def part1(path) do
     mat = input_to_matrix(path)
     mat
@@ -93,6 +118,18 @@ defmodule Day4 do
       row_sum = row
       |> Enum.with_index()
       |> Enum.reduce(0, fn {_, x}, acc2 -> match_str(mat, {x, y}, ["X", "M", "A", "S"]) + acc2 end)
+      row_sum + acc1
+    end)
+  end
+
+  def part2(path) do
+    mat = input_to_matrix(path)
+    mat
+    |> Enum.with_index()
+    |> Enum.reduce(0, fn {row, y}, acc1 ->
+      row_sum = row
+      |> Enum.with_index()
+      |> Enum.reduce(0, fn {_, x}, acc2 -> cross_match_str(mat, {x, y}, ["M", "A", "S"]) + acc2 end)
       row_sum + acc1
     end)
   end
