ea7dd4db0fa5cdd777ba217c132741c4d47399ff
AVR-GCC <ogoun.d@gmail.com>
2025-10-27 15:19:13 +0200

    Pivot sort with compare function

--- lib/elixir_aoc.ex
+++ lib/elixir_aoc.ex
@@ -1,16 +1,22 @@
 defmodule ElixirAoc do
-  defp pivot_one_item(acc, _, []), do: acc
-  defp pivot_one_item({less, more}, item, [head | tail]) do
-    if item < head do
-      pivot_one_item({less, [head | more]}, item, tail)
+  defp pivot_one_item(_, acc, _, []), do: acc
+  defp pivot_one_item(fun, {less, more}, item, [head | tail]) do
+    if fun.(item, head) do
+      pivot_one_item(fun, {less, [head | more]}, item, tail)
     else
-      pivot_one_item({[head | less], more}, item, tail)
+      pivot_one_item(fun, {[head | less], more}, item, tail)
     end
+  end
+
+  def pivot_sort(_, []), do: []
+  def pivot_sort(fun, [head | tail]) do
+    {less, more} = pivot_one_item(fun, {[], []}, head, tail)
+    Enum.concat(pivot_sort(fun, less), [head | pivot_sort(fun, more)])
   end
 
   def pivot_sort([]), do: []
   def pivot_sort([head | tail]) do
-    {less, more} = pivot_one_item({[], []}, head, tail)
+    {less, more} = pivot_one_item(&</2, {[], []}, head, tail)
     Enum.concat(pivot_sort(less), [head | pivot_sort(more)])
   end
 
